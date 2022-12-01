ca359bd28175bced90679a234513a4704cabf20f
Kyle Coffey <me@phytolizer.dev>
2022-12-01 10:06:07 -0600

    improve efficiency by only storing top N best

--- src/aoc2022pkg/dec01.nim
+++ src/aoc2022pkg/dec01.nim
@@ -3,28 +3,43 @@ import std/[
   strutils,
 ]
 
-proc insertSorted(s: var seq[int], item: int) =
-  var i = 0
-  while i < s.len and s[i] < item:
-    inc i
-  s.insert(item, i)
+type MaxList = object
+  n: int
+  list: seq[int]
+
+proc newMaxList(n: int): MaxList =
+  result.n = n
+  result.list = newSeq[int](n)
+
+proc add(self: var MaxList, x: int) =
+  if self.list.len == 0:
+    self.list.add(x)
+    return
+
+  var insertIndex = self.list.len
+  for (i, v) in self.list.pairs:
+    if x <= v:
+      insertIndex = i
+      break
+
+  self.list.insert(x, insertIndex)
+  if self.list.len > self.n:
+    self.list = self.list[1 .. ^1]
 
 proc run*(input: string, part: int): string =
   var currTotal = 0
-  var bestTotals = newSeq[int]()
+  let n = case part
+    of 1: 1
+    of 2: 3
+    else: raiseAssert "Invalid part " & $part
+  var bestTotals = newMaxList(n)
   for line in input.splitLines:
     if line.isEmptyOrWhitespace:
-      bestTotals.insertSorted(currTotal)
+      bestTotals.add(currTotal)
       currTotal = 0
       continue
 
     let calories = line.parseInt()
     currTotal += calories
 
-  case part
-  of 1:
-    return $bestTotals[^1]
-  of 2:
-    return $sum(bestTotals[^3 .. ^1])
-  else:
-    raiseAssert "invalid part " & $part
+  return $bestTotals.list.sum()
