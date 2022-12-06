450a4dcd40b2dd9799cb094471123c3ee4a2c9bc
Kyle Coffey <me@phytolizer.dev>
2022-12-06 09:51:34 -0600

    extremely short

--- src/aoc2022pkg/dec06.nim
+++ src/aoc2022pkg/dec06.nim
@@ -1,23 +1,8 @@
 import std/[
   deques,
+  sequtils,
   sets,
 ]
-
-template `^^`(s: untyped, i: untyped): untyped =
-  when i is BackwardsIndex:
-    s.len - int(i)
-  else:
-    int(i)
-
-proc `[]`[T, I: Ordinal, J: Ordinal](d: Deque[T], x: HSlice[I, J]): seq[T] =
-  let a = d ^^ x.a
-  let L = (d ^^ x.b) - a + 1
-  result = newSeq[T](L)
-  for i in 0 ..< L:
-    result[i] = d[i + a]
-
-proc checkUnique(d: Deque[char], L: int): bool =
-  return d.len >= L and d[^L .. ^1].toHashSet.len == L
 
 proc run*(input: string, part: int): string =
   let uniqueLen = if part == 1:
@@ -27,5 +12,7 @@ proc run*(input: string, part: int): string =
   var stream = initDeque[char](uniqueLen)
   for (i, c) in input.pairs:
     stream.addLast c
-    if stream.checkUnique(uniqueLen):
+    if stream.len > uniqueLen:
+      stream.shrink(fromFirst = stream.len - uniqueLen)
+    if stream.items.toSeq.toHashSet.len == uniqueLen:
       return $(i + 1)
