662c63234cddb7f18f49baa7acc3331387066946
Kyle Coffey <me@phytolizer.dev>
2022-12-06 10:01:16 -0600

    tweak whitespace to waste less

--- src/aoc2022pkg/dec06.nim
+++ src/aoc2022pkg/dec06.nim
@@ -5,10 +5,9 @@ import std/[
 ]
 
 proc run*(input: string, part: int): string =
-  let uniqueLen = if part == 1:
-    4
-  else:
-    14
+  let uniqueLen =
+    if part == 1: 4
+    else: 14
   var stream = initDeque[char](uniqueLen)
   for (i, c) in input.pairs:
     stream.addLast c
